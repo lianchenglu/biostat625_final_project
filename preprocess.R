9ebf6e0a07a3babf59560e22b252f2e52a629ee4
lianchenglu <chenggg@umich.edu>
2023-11-25 16:42:14 -0500

    R

--- preprocess.R
+++ preprocess.R
@@ -4,6 +4,7 @@ library(data.table)
 # Set seed and working directory
 set.seed(1234)
 # setwd('/home/chenggg/BIOSTAT625/project')
+start.time <- Sys.time()
 # Read data
 GPL13534 <- fread('data/GPL13534-11288.txt')
 # Efficient string splitting and data manipulation
@@ -18,32 +19,38 @@ annotated_probe <-
 annotated_probe <- unique(annotated_probe)
 # Optional: Convert to matrix if necessary
 annotated_probe <- as.matrix(annotated_probe)
+end.time <- Sys.time()
+cat('Time for fread annotated data: ', end.time - start.time, '\n')
+
 rm(GPL13534)
 gc()
+
+start.time <- Sys.time()
+
 data <- read.delim('data/GSE72556_series_matrix.txt', sep = '!')
 names(data) <- c("cg", "SampleCharacteristics")
 
-# 定义一个函数来处理和分割数据
+# Define a function to process and split the data
 process_data <- function(data, pattern, remove_pattern, is_numeric = TRUE) {
-  # 提取符合模式的数据
+  # Extract data that matches the pattern
   relevant_data <- data[grepl(pattern, data$SampleCharacteristics), "SampleCharacteristics"]
-  # 移除不需要的文本
+  # Remove unwanted text
   relevant_data <- gsub(remove_pattern, "", relevant_data)
-  # 额外的清洗步骤，去除制表符等
-  relevant_data <- gsub("\\t", "", relevant_data) # 去除制表符
-  relevant_data <- gsub("Sample_characteristics_ch1", "", relevant_data) # 去除特定字符串
-  # 分割数据
+  # Additional cleaning steps, TAB removal, etc
+  relevant_data <- gsub("\\t", "", relevant_data) # Remove the TAB
+  relevant_data <- gsub("Sample_characteristics_ch1", "", relevant_data) # Remove specific string
+  # Split data
   split_data <- unlist(strsplit(relevant_data, " "))
-  # 根据 is_numeric 参数决定是否转换为数值
+  # Determine whether to convert to a numeric value based on the is numeric parameter
   if (is_numeric) {
     split_data <- as.numeric(split_data)
-    # 移除NA值
+    # remove NA
     split_data <- split_data[!is.na(split_data)]
   }
   return(split_data)
 }
 
-# 应用函数
+# apply the function
 child_bmi <- process_data(data, "child bmi:", "child bmi:")
 adult_bmi <- process_data(data, "adult bmi:", "adult bmi:")
 child_waist <- process_data(data, "child waist:", "child waist:")
@@ -51,26 +58,29 @@ adult_waist <- process_data(data, "adult waist:", "adult waist:")
 gender <- process_data(data, "child gender:", "child gender:", is_numeric = FALSE)[-1]
 age <- process_data(data, "child age:", "child age:")
 
-# 处理accession number
+# Processing accession number
 accession <- process_data(data, "GSM", "", is_numeric = FALSE)[1:96]
 accession[1] <- 'GSM1865141'
 accession <- subset(accession, accession != "GSM1865188")
 accession <- subset(accession, accession != "GSM1865221")
 accession <- subset(accession, accession != "GSM1865231")
 
-# 创建数据框
+# Create data frame
 adult_child <- data.frame(accession, adult_bmi, child_bmi, adult_waist, child_waist, gender, age)
 
-library(data.table)
-# 读取数据
-y1 <- fread('data/GSE72556_series_matrix.txt', header = FALSE, fill = TRUE)
-y2 <- y1
-# 移除前65行的注释
-y1 <- tail(y2, -65)
+end.time <- Sys.time()
+cat('Time for optimization: ', end.time - start.time, '\n')
 
-# 转置数据
-y1 <- transpose(y1)
-
-# 设置第一列为列名,第一行为行名,然后删除
-colnames(y1) <- as.character(y1[1, ])
-y1 <- y1[-1, ]
+# library(data.table)
+# # read data
+# y1 <- fread('data/GSE72556_series_matrix.txt', header = FALSE, fill = TRUE)
+# y2 <- y1
+# # Remove comments from the first 65 lines
+# y1 <- tail(y2, -65)
+# 
+# # Transposed data
+# y1 <- transpose(y1)
+# 
+# # Set the first column name, the first row name, and then delete
+# colnames(y1) <- as.character(y1[1, ])
+# y1 <- y1[-1, ]
